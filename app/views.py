392581072f5e1932eac9668a5bb7ef1849d274aa
Vincent Girard-Reydet <vincent.girardreydet@gmail.com>
2017-12-03 22:01:30 +0100

    Add templates

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    """Displays the root page of the application"""
+    user = {'nickname': 'Vincent'}  # fake user
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
